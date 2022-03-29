4e0d79a6ee2c090cde653b42e283d50ac72db00c
Ansarr24 <ansarr97@gmail.com>
2022-03-29 09:39:08 +0800

    buah header

--- resources/views/users/KilangBuah/buah-bahagian-vi.blade.php
+++ resources/views/users/KilangBuah/buah-bahagian-vi.blade.php
@@ -55,7 +55,7 @@
 
                                     <div class="mb-5 text-center">
                                         {{-- <img src="{{ asset('/mpob.png') }}" height="80" class='mb-4'> --}}
-                                        <h3 style="color: rgb(39, 80, 71); margin-bottom:1%">Bahagian VI</h3>
+                                        <h3 style="color: rgb(39, 80, 71); margin-bottom:1%">Bahagian 6</h3>
                                         <h5 style="color: rgb(39, 80, 71); font-size:14px">Eksport Produk Sawit
                                         </h5>
                                         {{-- <p>Maklumat Kilang</p> --}}
