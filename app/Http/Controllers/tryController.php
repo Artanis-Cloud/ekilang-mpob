8bbafe0aa4913e659bfb57f7f9386c8a3ffca4be
Syazwani Saliman <93377184+syn995@users.noreply.github.com>
2022-08-26 16:35:47 +0800

    Update tryController.php

--- app/Http/Controllers/tryController.php
+++ app/Http/Controllers/tryController.php
@@ -14,22 +14,23 @@ class tryController extends Controller
         return view('loginArx');
     }
     public function testing3(){
-        $loginmills = DB::connection('mysql3')->select("SELECT right(f.tahun,2), f.bulan,f.oer_cpo,d.oer_cpo,n.oer_cpo,s.oer_cpo,m.oer_cpo
-        FROM oerpelesen f, oerdaerah d, oernegeri n, oersemsia s, oermsia m
-        WHERE f.nolesen = '500008704000 ' AND
-        (f.tahun = '2017') AND
-        f.tahun = d.tahun AND
-        f.bulan = d.bulan AND
-        f.tahun = n.tahun AND
-        f.bulan = n.bulan AND
-        f.tahun = s.tahun AND
-        f.bulan = s.bulan AND
-        f.tahun = m.tahun AND
-        f.bulan = m.bulan AND
-        f.negeri = d.negeri AND
-        f.daerah = d.daerah AND
-        f.negeri = n.negeri
-        order by f.tahun, f.bulan");
+        // $loginmills = DB::connection('mysql3')->select("SELECT right(f.tahun,2), f.bulan,f.oer_cpo,d.oer_cpo,n.oer_cpo,s.oer_cpo,m.oer_cpo
+        // FROM oerpelesen f, oerdaerah d, oernegeri n, oersemsia s, oermsia m
+        // WHERE f.nolesen = '500008704000 ' AND
+        // (f.tahun = '2017') AND
+        // f.tahun = d.tahun AND
+        // f.bulan = d.bulan AND
+        // f.tahun = n.tahun AND
+        // f.bulan = n.bulan AND
+        // f.tahun = s.tahun AND
+        // f.bulan = s.bulan AND
+        // f.tahun = m.tahun AND
+        // f.bulan = m.bulan AND
+        // f.negeri = d.negeri AND
+        // f.daerah = d.daerah AND
+        // f.negeri = n.negeri
+        // order by f.tahun, f.bulan");
+        $loginmills = DB::connection('mysql3')->select("show databases");
         dd($loginmills);
         return view('users.users-dashboard');
     }
