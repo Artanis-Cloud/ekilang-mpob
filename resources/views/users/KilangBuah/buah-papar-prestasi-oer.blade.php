2134d00b50b23a6478821672fc2c8e60beaafc46
Syazwani Saliman <93377184+syn995@users.noreply.github.com>
2022-10-04 12:01:39 +0800

    Update buah-papar-prestasi-oer.blade.php

--- resources/views/users/KilangBuah/buah-papar-prestasi-oer.blade.php
+++ resources/views/users/KilangBuah/buah-papar-prestasi-oer.blade.php
@@ -83,7 +83,7 @@
                         // loop
                         ['Individu', {{ $individu }}],
                         ['Daerah', 130, 100, 130, 100, 130, 100],
-                        ['Negeri', {{ $negeri }}],
+                        ['Negeri', 12, 59, 12, 59, 12, 59],
                         ['Semenanjung Malaysia', 42, 25, 42, 25, 42, 25],
                         ['Malaysia', 76, 84, 76, 84, 76, 84],
                     ],
