caf0eb689ef30ca7f2e3ee0abefb9e13bcc40a16
Syazwani Saliman <93377184+syn995@users.noreply.github.com>
2022-10-12 10:39:28 +0800

    Update Proses9Controller.php

--- app/Http/Controllers/Admin/Proses9Controller.php
+++ app/Http/Controllers/Admin/Proses9Controller.php
@@ -567,7 +567,7 @@ class Proses9Controller extends Controller
                where
                      e.F911A = p.F201A and e.F911B = '$e91_nobatch'");
 
-                     dd($penyata[$key]);
+                    //  dd($penyata[$key]);
 
             // $penyata[$key]  = H91Init::with('pelesen')->whereRelation('pelesen','e_nl', $penyata_id[$key] ->e91_nl)->first();
             // $pelesens[$key] = Pelesen::where('e_nl', $penyata_id[$key] ->e91_nl)->first();
@@ -577,7 +577,7 @@ class Proses9Controller extends Controller
 
         }
 //   dd($pelesens);
-// dd($penyata[1][0]->nolesen);
+dd($penyata[$key]);
 
 
 
