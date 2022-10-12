83f21e230f80427c9b586d4fe35740c0cf50057e
Syazwani Saliman <93377184+syn995@users.noreply.github.com>
2022-10-12 11:32:18 +0800

    Update 9papar-pleid-buah-multi.blade.php

--- resources/views/admin/proses9/9papar-pleid-buah-multi.blade.php
+++ resources/views/admin/proses9/9papar-pleid-buah-multi.blade.php
@@ -62,7 +62,7 @@
 
                         @foreach ($query as $key => $data)
 
-                        {{-- {{ dd($data) }} --}}
+                        {{ dd($data[$key]) }}
 
                             <div class="card-body">
                                 {{-- <div class="row"> --}}
