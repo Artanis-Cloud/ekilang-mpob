3ad44a7deb704371ce16e4cfe8bfd2ff108d2145
Syazwani Saliman <93377184+syn995@users.noreply.github.com>
2022-11-07 11:02:17 +0800

    Update Proses9Controller.php

--- app/Http/Controllers/Admin/Proses9Controller.php
+++ app/Http/Controllers/Admin/Proses9Controller.php
@@ -800,7 +800,7 @@ class Proses9Controller extends Controller
         // $nolesen = auth()->users->username;
         foreach ($nobatch as $key => $nobatch1) {
             $pelesens[$key] = (object)[];
-            $e_nl = H101Init::where('e101_nobatch', $nobatch1)->get();
+            $e_nl = H101Init::where('e101_nobatch', $nobatch1)->first();
 
             $query = H101Init::with('h_pelesen')->where('e_nl', $e_nl->e_nl)->first();
             dd($query);
@@ -877,7 +877,7 @@ class Proses9Controller extends Controller
         // dd($penyata);
         // $data = DB::table('pelesen')->get();
         return view('admin.proses9.9papar-pleid-buah-multi', compact(
-            'returnArr', 'layout', 'query', 'pelesens',
+            'returnArr', 'layout', 'query', 'pelesens', 'e_nl'
             'penyata1',
             'penyata2',
             'penyata3',
