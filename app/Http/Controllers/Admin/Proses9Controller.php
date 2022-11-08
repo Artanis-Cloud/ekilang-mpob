0b75324e1136068ccc93233bbdfd447b751f1534
Syazwani Saliman <93377184+syn995@users.noreply.github.com>
2022-11-08 15:34:45 +0800

    101

--- app/Http/Controllers/Admin/Proses9Controller.php
+++ app/Http/Controllers/Admin/Proses9Controller.php
@@ -897,7 +897,7 @@ class Proses9Controller extends Controller
 
                 // H101Init::with('h_pelesen')->where('e101_nobatch', $nobatch1)->where('e101_thn', $tahun)->where('e101_bln', $bulan)->first();
                 // dd($query);
-                $users = DB::connection('mysql4')->select("SELECT DATE_FORMAT(e.F101A2, '%d-%m-%Y') tkhsubmit from pl101ap3 e where e.F101A4 = '$nobatch1'");
+                $users[$nobatch1] = DB::connection('mysql4')->select("SELECT DATE_FORMAT(e.F101A2, '%d-%m-%Y') tkhsubmit from pl101ap3 e where e.F101A4 = '$nobatch1'");
 
 
                 $penyata1[$nobatch1] = DB::connection('mysql4')->select("SELECT p.comm_desc, e.F101B4, e.F101B5, e.F101B6, e.F101B7, e.F101B8, e.F101B9,
