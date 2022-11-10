b2f7d0ae2edcd241bb49af123a657a37188ff080
Syazwani Saliman <93377184+syn995@users.noreply.github.com>
2022-11-10 12:56:11 +0800

    Update Proses9Controller.php

--- app/Http/Controllers/Admin/Proses9Controller.php
+++ app/Http/Controllers/Admin/Proses9Controller.php
@@ -2196,7 +2196,7 @@ class Proses9Controller extends Controller
                 }
                 $layout = 'layouts.main';
 
-                dd($bhga);
+                // dd($bhga);
                 // $data = DB::table('pelesen')->get();
                 return view('admin.proses9.9papar-pleid-simpanan-multi', compact(
                     'returnArr', 'bulan', 'tahun',
