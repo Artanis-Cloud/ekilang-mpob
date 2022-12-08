af7d7da444809fd22f4c9e981f56db36610000b5
Ansarr24 <ansarr97@gmail.com>
2022-12-08 10:53:54 +0800

    button kembali

--- resources/views/admin/proses9/9papar-terdahulu-isirung-multi.blade.php
+++ resources/views/admin/proses9/9papar-terdahulu-isirung-multi.blade.php
@@ -43,7 +43,8 @@
 
                 <div class="row" style="padding: 20px; background-color: white; margin-right:2%; margin-left:2%">
                     <div class="col-1 align-self-center">
-                        <a href="{{ $returnArr['kembali'] }}" class="btn" style=" color:rgb(64, 69, 68)"><i class="fa fa-angle-left">&ensp;</i>Kembali</a>
+                        <a href="javascript:history.back()" class="btn" style=" color:rgb(64, 69, 68)"><i
+                                class="fa fa-angle-left">&ensp;</i>Kembali</a>
                     </div>
 
                     <div class="col-11 align-self-center" style="text-align: right">
