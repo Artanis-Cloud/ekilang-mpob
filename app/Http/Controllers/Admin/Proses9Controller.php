a5d210a48ed411113af2a6b35c460d4f95970e0e
Syazwani Saliman <93377184+syn995@users.noreply.github.com>
2023-02-21 11:14:47 +0800

    Update Proses9Controller.php

--- app/Http/Controllers/Admin/Proses9Controller.php
+++ app/Http/Controllers/Admin/Proses9Controller.php
@@ -1079,7 +1079,7 @@ class Proses9Controller extends Controller
             } elseif ($tahun > 2022) {
                 $check = H101Init::with('h_pelesen')->where('e101_nobatch', $nobatch)->where('e101_thn', $tahun)->where('e101_bln', $bulan)->first();
 
-                    dd($check->pelesen->e_bln);
+                    dd($check->h_pelesen->e_bln);
                     // dd($check->h_pelesen->e_bln >= 1 && $check->h_pelesen->e_bln < 10);
                 // foreach ($checks as $check) {
                         # code...
