fb543142e345b76dca68148b6fc76d2e7f0a85c7
Syazwani Saliman <ansarr97@gmail.com>
2023-03-28 15:55:57 +0800

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -36,7 +36,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(RoutingUrlGenerator $url)
     {
 
-        if (strstr(URL::current(),'10.0.2.173')){
+        if (strstr(URL::current(),env("SERVER_IP_PRIVATE"))){
             $url->forceScheme('http');
        }
        else
